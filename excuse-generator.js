e9a546111842476d0e1351a29a4d496615f5c5e9
Settar Mengli <settar.mengli@gmail.com>
2026-03-11 03:23:41 -0400

    Completed excuse generator project

---
+++ excuse-generator.js
@@ -0,0 +1,11 @@
+const who = ["My dog", "My grandma", "My neighbor", "A stranger"];
+const action = ["ate", "broke", "stole", "destroyed"];
+const what = ["my homework", "my phone", "my car", "my laptop"];
+const when = ["today", "yesterday", "last night", "this morning"];
+
+const randomWho = who[Math.floor(Math.random() * who.length)];
+const randomAction = action[Math.floor(Math.random() * action.length)];
+const randomWhat = what[Math.floor(Math.random() * what.length)];
+const randomWhen = when[Math.floor(Math.random() * when.length)];
+const excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+console.log(excuse);
